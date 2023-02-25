f0b46a9806dfbbc218133a2cf2f01491d90bf5e7
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-25 17:51:35 +0100

    validity duration based lease constructor

--- src/lease.rs
+++ src/lease.rs
@@ -1,8 +1,17 @@
 use std::net::Ipv4Addr;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
 #[derive(Clone, Debug)]
 pub struct Lease {
     pub address: Ipv4Addr,
     pub expires: SystemTime,
+}
+
+impl Lease {
+    pub fn new(address: Ipv4Addr, lease_time: Duration) -> Self {
+        Self {
+            address,
+            expires: SystemTime::now() + lease_time,
+        }
+    }
 }
