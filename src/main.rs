4d90e0c61784b1d507047bceb323d0ed7dc47318
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-25 18:11:51 +0100

    basic packet acceptor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, UdpSocket};
+
+fn main() -> io::Result<()> {
+    let sock = UdpSocket::bind("0.0.0.0:67")?;
+
+    sock.set_broadcast(true)?;
+
+    loop {
+        let mut buf = [0; 1024];
+        let (n, remote) = sock.recv_from(&mut buf)?;
+        let buf = &buf[..n];
+
+        let remote = match remote.ip() {
+            IpAddr::V4(addr) => addr,
+            _ => {
+                unreachable!();
+            }
+        };
+
+        match handle_request(buf, remote) {
+            Ok(_) => {}
+            Err(e) => eprintln!("erroneous request from {}: {}", remote, e),
+        }
+    }
+}
+
+fn handle_request(buf: &[u8], remote: Ipv4Addr) -> anyhow::Result<()> {
+    todo!();
 }
