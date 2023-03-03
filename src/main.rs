ccc86e55e53dce9c67b2b902adea394955d4e515
HimbeerserverDE <himbeerserverde@gmail.com>
2023-03-03 17:18:21 +0100

    don't print redundant client address information
    the client is probably sending from 0.0.0.0 anyway

--- src/main.rs
+++ src/main.rs
@@ -43,22 +43,20 @@ fn handle_request(sock: &UdpSocket, buf: &[u8], remote: SocketAddrV4) -> anyhow:
             let xid = msg.xid();
             let opts = msg.opts();
 
-            let msg_type = opts
-                .msg_type()
-                .ok_or(anyhow!("no message type given from {}", remote))?;
+            let msg_type = opts.msg_type().ok_or(anyhow!("no message type given"))?;
 
             match msg_type {
                 MessageType::Discover => {
                     let client_id = match opts
                         .get(OptionCode::ClientIdentifier)
-                        .ok_or(anyhow!("no client id from {}", remote))?
+                        .ok_or(anyhow!("no client id"))?
                     {
                         DhcpOption::ClientIdentifier(id) => id,
-                        _ => bail!("expected ClientIdentifier from {}", remote),
+                        _ => bail!("expected ClientIdentifier"),
                     };
 
                     let free_addr = choose_free_address(&lease_mgr, client_id)
-                        .ok_or(anyhow!("no free addresses available for {}", remote))?;
+                        .ok_or(anyhow!("no free addresses available"))?;
 
                     let mut resp = Message::default();
                     let opts = resp
@@ -77,22 +75,15 @@ fn handle_request(sock: &UdpSocket, buf: &[u8], remote: SocketAddrV4) -> anyhow:
                     let n = sock.send_to(&resp_buf, remote)?;
 
                     if n != resp_buf.len() {
-                        Err(anyhow!(
-                            "message length inconsistency sending to {}",
-                            remote
-                        ))
+                        Err(anyhow!("partial response"))
                     } else {
                         Ok(())
                     }
                 }
-                _ => Err(anyhow!(
-                    "invalid message type {:?} from {}",
-                    msg_type,
-                    remote
-                )),
+                _ => Err(anyhow!("invalid message type {:?}", msg_type,)),
             }
         }
-        _ => Err(anyhow!("invalid opcode {:?} from {}", op, remote)),
+        _ => Err(anyhow!("invalid opcode {:?}", op)),
     }
 }
 
