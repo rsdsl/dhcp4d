29b2705a344467c92dfa3f2ea65ab536c96fe74e
HimbeerserverDE <himbeerserverde@gmail.com>
2023-10-14 14:22:49 +0200

    make error enum style consistent with my convention

--- src/error.rs
+++ src/error.rs
@@ -7,40 +7,42 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("no ipv4 addr on interface {0}")]
-    NoIpv4Addr(String),
-    #[error("unhandled or unknown opcode {0:?}")]
-    InvalidOpcode(Opcode),
-    #[error("missing message type")]
-    NoMsgType,
-    #[error("unhandled or unknown message type {0:?}")]
-    InvalidMsgType(MessageType),
-    #[error("missing client id")]
-    NoClientId,
     #[error("empty client id")]
     EmptyClientId,
+    #[error("unhandled or unknown message type {0:?}")]
+    InvalidMsgType(MessageType),
+    #[error("unhandled or unknown opcode {0:?}")]
+    InvalidOpcode(Opcode),
     #[error("missing ip address in dhcprequest")]
     NoAddrRequested,
-    #[error("addr pool exhausted")]
-    PoolExhausted,
+    #[error("missing client id")]
+    NoClientId,
+    #[error("no ipv4 addr on interface {0}")]
+    NoIpv4Addr(String),
+    #[error("missing message type")]
+    NoMsgType,
     #[error("bytes sent not equal to pkt size")]
     PartialResponse,
-    #[error("dhcproto encode error")]
-    DhcprotoEncode(#[from] dhcproto::error::EncodeError),
-    #[error("dhcproto decode error")]
-    DhcprotoDecode(#[from] dhcproto::error::DecodeError),
+    #[error("addr pool exhausted")]
+    PoolExhausted,
+
+    #[error("ip addr parse error")]
+    AddrParseError(#[from] net::AddrParseError),
+    #[error("ffi nul error (string contains nul bytes)")]
+    FfiNulError(#[from] ffi::NulError),
     #[error("io error")]
     Io(#[from] io::Error),
+
+    #[error("dhcproto decode error")]
+    DhcprotoDecode(#[from] dhcproto::error::DecodeError),
+    #[error("dhcproto encode error")]
+    DhcprotoEncode(#[from] dhcproto::error::EncodeError),
     #[error("linkaddrs error")]
     LinkAddrs(#[from] linkaddrs::Error),
     #[error("rsdsl_netlinkd error")]
     RsdslNetlinkd(#[from] rsdsl_netlinkd::error::Error),
     #[error("serde_json error")]
     SerdeJson(#[from] serde_json::Error),
-    #[error("ip addr parse error")]
-    AddrParseError(#[from] net::AddrParseError),
-    #[error("ffi nul error (string contains nul bytes)")]
-    FfiNulError(#[from] ffi::NulError),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
